cc70472254b35e44443d3c47067a11901a7df0a2
NSING VICTORY EBAJI <84423032+venvictor602@users.noreply.github.com>
2023-07-26 14:32:27 +0100

    Update settings.py
    link chnage

--- bitcoin/settings.py
+++ bitcoin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oq%ihp9_04q*gtqejedxyz4n!a7h$xyqe&mb&k87^sw$no1dc9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.quadchain.live', '.vercel.app','.now.sh','127.0.0.1','localhost', 'https://www.quadchain.live' ]
+ALLOWED_HOSTS = [ '.vercel.app','.now.sh','127.0.0.1','localhost', 'https://www.quadchain.live' ]
 
 
 # Application definition
